baec6cfbd3e12ba1708a6667a01bf7ae51f9e2d8
Alejandro <10653@holbertonstudents.com>
2025-04-23 23:57:42 +0000

    ajustando Betty Styles

--- buitlins.c
+++ buitlins.c
@@ -1,21 +1,36 @@
 #include "shell.h"
 
+/**
+ * handle_exit - Handles the exit command for the shell
+ * @args: Array of command arguments
+ * @input: Input string to be freed
+ *
+ * Description: Frees allocated memory and exits the shell
+ * Return: None (exits process)
+ */
 void handle_exit(char **args, char *input)
 {
-    if (args)
-        free(args);
-    if (input)
-        free(input);
-    exit(0);
+	if (args)
+		free(args);
+	if (input)
+		free(input);
+	exit(0);
 }
 
+/**
+ * handle_env - Prints all environment variables
+ *
+ * Description: Iterates through the environ array and prints
+ *              each environment variable
+ * Return: None
+ */
 void handle_env(void)
 {
-    char **env = environ;
+	char **env = environ;
 
-    while (*env)
-    {
-        printf("%s\n", *env);
-        env++;
-    }
+	while (*env)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
 }
--- execute.c
+++ execute.c
@@ -1,54 +1,71 @@
 #include "shell.h"
 
+/**
+ * execute_command - Executes a command based on input arguments
+ * @args: Array of command arguments
+ * @input: Input string (for freeing in exit command)
+ *
+ * Description: Checks if the command is built-in (exit, env) and executes
+ * accordingly, or passes it to external command handler
+ * Return: None
+ */
 void execute_command(char **args, char *input)
 {
-    if (strcmp(args[0], "exit") == 0)
-    {
-        handle_exit(args, input);
-    }
-    else if (strcmp(args[0], "env") == 0)
-    {
-        handle_env();
-    }
-    else
-    {
-        execute_external_command(args);
-    }
+	if (strcmp(args[0], "exit") == 0)
+	{
+		handle_exit(args, input);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		handle_env();
+	}
+	else
+	{
+		execute_external_command(args);
+	}
 }
 
+/**
+ * execute_external_command - Executes an external command
+ * @args: Array of command arguments
+ *
+ * Description: Forks a child process to execute the command
+ * either using the direct path or searching in PATH
+ * Return: None
+ */
 void execute_external_command(char **args)
 {
-    pid_t pid;
-    char *command_path = args[0];
+	pid_t pid;
+	char *command_path = args[0];
 
-    if (access(command_path, X_OK) != 0)
-    {
-        command_path = find_command_in_path(args[0]);
-        if (!command_path)
-        {
-            print_command_error(args[0]);
-            exit(127);
-        }
-    }
+	if (access(command_path, X_OK) != 0)
+	{
+		command_path = find_command_in_path(args[0]);
+		if (!command_path)
+		{
+			print_command_error(args[0]);
+			exit(127);
+		}
+	}
 
-    pid = fork();
-    if (pid == 0)
-    {
-        if (execve(command_path, args, environ) == -1)
-        {
-            perror(command_path);
-            exit(127);
-        }
-    }
-    else if (pid > 0)
-    {
-        wait(NULL);
-    }
-    else
-    {
-        perror("Fork error");
-    }
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(command_path, args, environ) == -1)
+		{
+			perror(command_path);
+			exit(127);
+		}
+	}
+	else if (pid > 0)
+	{
+		wait(NULL);
+	}
+	else
+	{
+		perror("Fork error");
+	}
 
-    if (command_path != args[0])
-        free(command_path);
+	if (command_path != args[0])
+		free(command_path);
 }
