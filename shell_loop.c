1956ce2f6a22dfd8884a7f170c9f9f2550942147
Alejandro <10653@holbertonstudents.com>
2025-04-24 00:10:48 +0000

    Correcciones de Betty

--- shell_loop.c
+++ shell_loop.c
@@ -1,36 +1,43 @@
 #include "shell.h"
 
+/**
+ * run_shell_loop - Main loop for the shell program
+ *
+ * Description: Continuously reads input from the user, processes it,
+ * and executes the appropriate commands until exit
+ * Return: None
+ */
 void run_shell_loop(void)
 {
-    char *input = NULL;
-    size_t len = 0;
-    ssize_t read;
-    char **args;
-    int is_interactive = isatty(STDIN_FILENO);
+	char *input = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char **args;
+	int is_interactive = isatty(STDIN_FILENO);
 
-    while (1)
-    {
-        if (is_interactive)
-            print_prompt();
+	while (1)
+	{
+		if (is_interactive)
+			print_prompt();
 
-        read = getline(&input, &len, stdin);
-        if (read == -1)
-        {
-            if (is_interactive)
-                printf("\n");
-            break;
-        }
+		read = getline(&input, &len, stdin);
+		if (read == -1)
+		{
+			if (is_interactive)
+				printf("\n");
+			break;
+		}
 
-        args = tokenize_input(input);
-        if (!args || !args[0])
-        {
-            free(args);
-            continue;
-        }
+		args = tokenize_input(input);
+		if (!args || !args[0])
+		{
+			free(args);
+			continue;
+		}
 
-        execute_command(args, input);
-        free(args);
-    }
+		execute_command(args, input);
+		free(args);
+	}
 
-    free(input);
+	free(input);
 }
